fbfe28b6aaf0759608e0dcfde7a4ac0ae8d86b81
Polyxen1a <maslowa@inbox.ru>
2023-01-31 22:00:47 +0300

    Введение в Maven и сборку проектов

---
+++ src/main/java/com/skypro/introductiontomaven/IntroductionToMavenApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.introductiontomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionToMavenApplication.class, args);
+    }
+
+}
